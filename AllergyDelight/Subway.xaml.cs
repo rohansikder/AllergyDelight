e83987bf64825d02beebc30f86c9820c8d0e578d
Rohan Sikder <Rohansikder4@gmail.com>
2022-03-21 20:14:55 +0000

    Added 2 new pages for Supermacs and Subway

---
+++ AllergyDelight/Subway.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AllergyDelight
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Subway : ContentPage
+    {
+        public Subway()
+        {
+            InitializeComponent();
+        }
+
+        async private void filter_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FilterPage());
+        }
+
+        async private void BackToHomeClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
