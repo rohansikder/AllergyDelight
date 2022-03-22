a366b05c0b792f24c1f7b2fcce851d0a3c9a2e45
ANTHONY SULTANOV <G00390720@gmit.ie>
2022-03-22 19:32:13 +0000

    Working on listpage

---
+++ AllergyDelight/ListPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AllergyDelight
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void filter_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FilterPage());
+        }
+
+
+
+        //Sends User to Resturants Page
+        private async void menu_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+        private async void tools_clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void mcdoanlds1_clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void mcdoanlds2_clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void supermacs1_clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void subway1_clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
