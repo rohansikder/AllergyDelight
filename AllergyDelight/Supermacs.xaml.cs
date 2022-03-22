2d8dcafb20f73c6008c15745f5b8a3cc176c335b
Rohan Sikder <Rohansikder4@gmail.com>
2022-03-22 18:56:05 +0000

    Added Hyperlinks in the Resturant Information Page

--- AllergyDelight/Supermacs.xaml.cs
+++ AllergyDelight/Supermacs.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,13 @@ namespace AllergyDelight
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Supermacs : ContentPage
     {
+
+        public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+
         public Supermacs()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
         async private void filter_clicked(object sender, EventArgs e)
