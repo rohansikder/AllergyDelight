39a90b8c08633daf6968c20b9f08338f16181dd4
Joshua Uzell <g00391557@gmit.ie>
2022-03-28 22:33:41 +0100

    Added the Brand page and the other pages to the app including brands own filter pages which are placeholder for this commit

---
+++ AllergyDelight/SupermacsMainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AllergyDelight
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SupermacsMainPage : ContentPage
+    {
+        public SupermacsMainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void filter_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SupermacsFilterPage());
+        }
+
+        private async void restaurants_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Supermacs());
+        }
+        private async void brand_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BrandPage());
+        }
+    }
+}
